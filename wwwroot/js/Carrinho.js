c6f477237bda1909f54185658aa1f5026371a838
Victor Domingues Dantas <vic.d.dantas61@gmail.com>
2023-03-19 01:15:20 -0300

    inserindo view model

--- wwwroot/js/Carrinho.js
+++ wwwroot/js/Carrinho.js
@@ -58,7 +58,7 @@ class Carrinho {
             contentType: 'application/json',
             data: JSON.stringify(data)
 
-        }).done(function (response));
+        }).done(function (response) { location.reload() });
     }
     
 }
